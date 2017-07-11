27fe23c2d6b4607260796ddaa5f0a03a8a6fa8b6
ttfreire <matheusfreire@gmail.com>
2017-07-11 14:17:46 -0300

    barco

---
+++ barco/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1499793350,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/water-sheet0.png",
+		"images/land-sheet0.png",
+		"images/boat-sheet0.png",
+		"images/good1-sheet0.png",
+		"images/bad1-sheet0.png",
+		"images/stopi-sheet0.png",
+		"images/seata-sheet0.png",
+		"images/jumpcollider-sheet0.png",
+		"images/landingpositionf-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
